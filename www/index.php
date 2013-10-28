38c7cd7c8521b7e4ff6593c6a9ed2be775140fbc
swearer23 <Swift.Young@yangshiweideMacBook-Air.local>
2013-10-28 14:37:54 +0800

    added index

---
+++ www/index.php
@@ -0,0 +1,11 @@
+<?php
+require '../phails/PhailStarter.php';
+function __autoload($classname){
+	Common::requireClass($classname);
+}
+$request = Common::getRequest();
+$controller = $request["controller"];
+$action = $request["action"];
+$class = new $controller;
+$class->$action();
+?>
