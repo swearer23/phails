87ad4a61accaa6679aa02b5df38cd9cfc1ddfd5b
swearer23 <Swift.Young@yangshiweideMacBook-Air.local>
2013-11-04 18:17:51 +0800

    default config environment

--- config/Environment.php
+++ config/Environment.php
@@ -1,2 +1,34 @@
 <?php
+class UserEnvironment{
+	public static $config = array(
+		"env" => Environment::DEV,
+		"dev" => array(
+			"dbConfig"	=> array(
+				"host"		=> "127.0.0.1",
+				"port"		=> "3306",
+				"database"	=> "",
+				"user"		=> "",
+				"pass"		=> ""
+			)
+		),
+		"production" => array(
+			"dbConfig"	=> array(
+				"host"		=> "127.0.0.1",
+				"port"		=> "3306",
+				"database"	=> "",
+				"user"		=> "",
+				"pass"		=> ""
+			)
+		),
+		"test" => array(
+			"dbConfig"	=> array(
+				"host"		=> "127.0.0.1",
+				"port"		=> "3306",
+				"database"	=> "",
+				"user"		=> "",
+				"pass"		=> ""
+			)
+		)
+	);
+}
 ?>
