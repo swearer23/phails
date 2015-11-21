2ea464ccbf28782cb493316c9c43c544db6ec2f2
shiwei.yang <swearer23@163.com>
2015-11-21 17:15:59 +0800

    start session when requesting index.php

--- www/index.php
+++ www/index.php
@@ -1,11 +1,15 @@
 <?php
+ob_start();
+date_default_timezone_set("Asia/Chongqing");
 require '../phails/PhailStarter.php';
 function __autoload($classname){
 	Common::requireClass($classname);
 }
 $request = Common::getRequest();
+session_start();
 $controller = $request["controller"];
 $action = $request["action"];
 $class = new $controller($request);
-$class->$action();
+$class->runAction($action);
+ob_end_flush();
 ?>
